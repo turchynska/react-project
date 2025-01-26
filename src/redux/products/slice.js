e878387062821a518659aedc34c59cd403cdc5bb
Kate <159571202+turchynska@users.noreply.github.com>
2025-01-25 22:30:36 -0500

    push redux
    redux to campers/ slice and operations

--- src/redux/products/slice.js
+++ src/redux/products/slice.js
@@ -1,6 +1,59 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchCampers, getCamperById } from "./operations.js";
 
-const dataUrl = axios.create({
-  baseURL: "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers",
+const campersSlice = createSlice({
+  name: "campers",
+  initialState,
+  reducers: {
+    clearItems: (state) => {
+      Object.assign(state, {
+        items: [],
+        total: 0,
+        page: 1,
+      });
+    },
+    setPage: (state) => {
+      state.page += 1;
+    },
+    addToFavorite: (state, { payload }) => {
+      state.favoriteItem.push(payload);
+    },
+    deleteFromFavorite: (state, { payload }) => {
+      state.favoriteItem = state.favoriteItem.filter(
+        ({ id }) => id !== payload
+      );
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCampers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchCampers.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(...payload.items);
+        state.total = payload.total;
+      })
+      .addCase(fetchCampers.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      })
+      .addCase(getCamperById.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.camper = payload;
+      })
+      .addCase(getCamperById.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCamperById.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      });
+  },
 });
+
+export const { clearItems, setPage, addToFavorite, deleteFromFavorite } = campersSlice.actions;
+export default campersSlice.reducer;
